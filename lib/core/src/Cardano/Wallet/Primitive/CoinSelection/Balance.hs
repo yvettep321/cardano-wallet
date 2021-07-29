5048e0571449c23d631605a6e61ea6e31f796554
Rodney Lorrimar <rodney.lorrimar@iohk.io>
2021-07-29 16:02:48 +0800

    Rework, refactor, allow partial signing
    Co-authored-by: Pawel Jakubas <pawel.jakubas@iohk.io>
    Co-authored-by: Rodney Lorrimar <rodney.lorrimar@iohk.io>

--- lib/core/src/Cardano/Wallet/Primitive/CoinSelection/Balance.hs
+++ lib/core/src/Cardano/Wallet/Primitive/CoinSelection/Balance.hs
@@ -99,6 +99,7 @@ module Cardano.Wallet.Primitive.CoinSelection.Balance
     , mapMaybe
     , balanceMissing
     , missingOutputAssets
+    , inputFromSelection
     ) where
 
 import Prelude
@@ -269,6 +270,10 @@ data SelectionResult change = SelectionResult
         -- the selection.
     }
     deriving (Generic, Eq, Show)
+
+-- | Make a transaction input resolver from the coin selection.
+inputFromSelection :: SelectionResult change -> TxIn -> Maybe TxOut
+inputFromSelection cs = flip lookup $ NE.toList $ view #inputsSelected cs
 
 -- | Calculate the actual difference between the total outputs (incl. change)
 -- and total inputs of a particular selection. By construction, this should be
