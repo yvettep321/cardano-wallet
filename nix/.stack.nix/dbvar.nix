2698f6d9b7807a7c3c9f483ba36896d8dc469050
Heinrich Apfelmus <heinrich.apfelmus@iohk.io>
2021-08-23 17:23:25 +0200

    Add new package `dbvar` to source tree

---
+++ nix/.stack.nix/dbvar.nix
@@ -0,0 +1,51 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = { release = false; };
+    package = {
+      specVersion = "1.10";
+      identifier = { name = "dbvar"; version = "2021.8.23"; };
+      license = "Apache-2.0";
+      copyright = "2022 IOHK";
+      maintainer = "operations@iohk.io";
+      author = "IOHK Engineering Team";
+      homepage = "https://github.com/input-output-hk/cardano-wallet";
+      url = "";
+      synopsis = "Utilities for a building Command-Line Interfaces";
+      description = "";
+      buildType = "Simple";
+      isLocal = true;
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+          (hsPkgs."io-classes" or (errorHandler.buildDepError "io-classes"))
+          ];
+        buildable = true;
+        };
+      tests = {
+        "unit" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."io-classes" or (errorHandler.buildDepError "io-classes"))
+            (hsPkgs."io-sim" or (errorHandler.buildDepError "io-sim"))
+            (hsPkgs."hspec" or (errorHandler.buildDepError "hspec"))
+            (hsPkgs."QuickCheck" or (errorHandler.buildDepError "QuickCheck"))
+            ];
+          build-tools = [
+            (hsPkgs.buildPackages.hspec-discover.components.exes.hspec-discover or (pkgs.buildPackages.hspec-discover or (errorHandler.buildToolDepError "hspec-discover:hspec-discover")))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    } // rec { src = (pkgs.lib).mkDefault ../.././lib/dbvar; }
